7bd71d9ca8a03072a050f9f3c14a32d2ffa1238e
Albert Guo <guozhaonan@gmail.com>
2015-09-16 19:10:31 -0400

    Last Update

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  unauthenticated :user do
-    devise_scope :user do
-      get "/" => "devise/sessions#new"
-    end
-  end
-
   resources :conversations do
     resources :messages
   end
