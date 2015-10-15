72aa3159caf8b02e5ad6f68c32991247ddf5e797
Myron Leung <myronleung@gmail.com>
2015-10-15 15:16:06 -0400

    added user_id, need to translate from id to first_name last_name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def create
+    @user = current_user
     @idea = Idea.find(params[:idea_id])
     @comment = @idea.comments.create(comment_params)
     redirect_to idea_path(@idea)
@@ -7,6 +10,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:user_id, :comment_text)
+      params.require(:comment).permit(:comment_text).merge(user_id: current_user.id)
     end
 end
