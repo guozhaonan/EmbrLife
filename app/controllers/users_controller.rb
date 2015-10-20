b03725693ccffd3e46da1ee98df2d3d90d3758c6
Albert Guo <guozhaonan@gmail.com>
2015-10-19 20:25:33 -0400

    Fixed Icons and Added Fontawesome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :username, :email, :college, :bio, :values, :interests, idea_ids: [])
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :college, :bio, :values, :interests, :year)
     end
 
 end
